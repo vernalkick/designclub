11142610e1773a764a0e21a183cec5403a8b38e0
Kevin Clark <kevin@kevinclark.ca>
2020-11-30 11:18:24 -0500

    Remove subfolder

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "cfbc5b248ff39cd79cac";
+/******/ 	var hotCurrentHash = "21a130760cef71ba8649";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
