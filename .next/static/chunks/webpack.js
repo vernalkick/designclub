81e4ad5868f94fa65fb0a2c69ae915e94c8317a2
Kevin Clark <kevin@kevinclark.ca>
2020-11-30 11:26:17 -0500

    try font loader

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "21a130760cef71ba8649";
+/******/ 	var hotCurrentHash = "b7fc0fbc6f42dd50f2e2";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
